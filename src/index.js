98f7e79492bf88ee21383a9093f1ea73f13f2caf
Zsombor Makker <makker.zsombor@gmail.com>
2022-11-15 08:46:16 +0100

    Feladat 1 kesz

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import './style.css';
+    
+    // Feladat 1:
+    document.getElementById('osszes').addEventListener('click', async () => {
+        let response = await fetch('quotes.json');
+        let eredmeny = await response.json();
+        console.log('elso fel gomb');
 
+        // sorrend:
+        let abc = eredmeny.quotes.sort(function(a, b){
+            let anev = a.author.toUpperCase();
+            let bnev = b.author.toUpperCase();
+
+            if(anev < bnev){
+                return -1;
+            }else if(anev > bnev){
+                return 1;
+            }else {
+                return 0;
+            }
+        });
+        // kiiratas:
+        let quoteLista = document.getElementById('listaUl');
+        for (let q of abc){
+            let li = document.createElement('li');
+            li.textContent = q.id + ' ' + q.quote + ' ' + q.author;
+            quoteLista.appendChild(li);
+        }
+        
+
+    });
